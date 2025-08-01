3a58766e4aeb6f31419b451b6470e937087d6fb4
Joe Cichocki <jcichocki@soagility.com>
2025-08-01 10:43:00 -0400

    start of example

---
+++ src/main/java/com/wakefern/sb_demo/SbDemoApplication.java
@@ -0,0 +1,14 @@
+package com.wakefern.sb_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SbDemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SbDemoApplication.class, args);
+    }
+
+}
